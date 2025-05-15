49693ed072f12bda056767c2273d5485d6386021
Cliff Wulfman <cwulfman@princeton.edu>
2025-05-15 14:48:04 -0400

    rewritten Pipe

--- src/pipeline/filters/mover.py
+++ src/pipeline/filters/mover.py
@@ -9,6 +9,8 @@ from pathlib import Path
 
 from pipeline.plumbing import InPipe, OutPipe, Filter
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+
 logger: logging.Logger = logging.getLogger(__name__)
 
 class Mover(Filter):
@@ -18,10 +20,12 @@ class Mover(Filter):
     def validate_token(self, token) -> bool:
         status:bool = True
         if Path(token.content['source_file']).exists() is False:
+            logging.error(f"source file does not exist: {token.content['source_file']}")
             self.log_to_token(token, "ERROR", f"source file does not exist: {token.content['source_file']}")
             status = False
 
         if Path(token.content['destination_file']).exists() is True:
+            logging.error(f"destination file already exists: {token.content['destination_file']}")
             self.log_to_token(token, "ERROR", "destination file already exists")
             status = False
         return status
@@ -40,3 +44,25 @@ class Mover(Filter):
             completed = False
 
         return completed
+
+
+
+if __name__ == '__main__':
+    # if 'GPG_PASSPHRASE' not in os.environ:
+    #     print("Please set the GPG_PASSPHRASE environment variable.")
+    #     sys.exit(1)
+
+    # from sys import argv
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output', required=True)
+    args = parser.parse_args()
+
+
+    input_pipe:InPipe = InPipe(args.input)
+    output_pipe:OutPipe = OutPipe(args.output)
+    mover:Mover = Mover(input_pipe, output_pipe)
+    logger.info("starting mover")
+    mover.run_forever()
--- src/pipeline/plumbing.py
+++ src/pipeline/plumbing.py
@@ -16,6 +16,9 @@ class Token:
         self.name = name
         self.content = content
 
+    def __repr__(self) -> str:
+        return f"Token({self.name})"
+
     def write_log(self, message:str, level:Optional[str] = None,  stage:Optional[str] = None):
         entry:dict = {"timestamp": str(datetime.now(timezone.utc)),
                       "message": message}
@@ -28,6 +31,88 @@ class Token:
 
 
 class Pipe:
+    def __init__(self, in_path:str, out_path:str) -> None:
+        self.input = Path(in_path)
+        self.output = Path(out_path)
+        self.token: Token | None = None
+
+    def __repr__(self) -> str:
+        return f"Pipe('{self.input}', '{self.output}')"
+
+
+    @property
+    def token_in_path(self) -> Path:
+         if self.token:
+            return self.input / Path(self.token.name).with_suffix('.json')
+         else:
+             raise ValueError("pipe doesn't contain a token")
+
+    @property
+    def token_out_path(self) -> Path:
+        if self.token:
+            return self.output / Path(self.token.name).with_suffix('.json')
+        else:
+            raise ValueError("pipe doesn't contain a token")
+
+    @property
+    def token_marked_path(self) -> Path:
+        if self.token:
+            return self.input / Path(self.token.name).with_suffix('.bak')
+        else:
+            raise ValueError("pipe doesn't contain a token")
+
+
+    @property
+    def token_error_path(self) -> Path:
+        if self.token:
+            return self.output / Path(self.token.name).with_suffix('.err')
+        else:
+            raise ValueError("pipe doesn't contain a token")
+
+    
+
+    def take_token(self):
+        if self.token:
+            logging.error("there's already a current token")
+            return None
+
+        try:
+            token_path = next(self.input.glob("*.json"))
+
+
+            with open(token_path, 'r') as f:
+                self.token = Token(json.load(f), token_path.name)
+
+            self.mark_token()
+            return self.token
+        
+        except StopIteration:
+            return None
+
+
+    def mark_token(self):
+        if self.token:
+            self.token_in_path.rename(self.token_marked_path)
+
+
+    def delete_marked_token(self):
+        if self.token_marked_path and self.token_marked_path.is_file():
+            self.token_marked_path.unlink()
+
+
+
+    def put_token(self) -> None:
+        if self.token:
+            with open(self.token_out_path, 'w') as f:
+                json.dump(self.token.content, f)
+            self.delete_marked_token()
+            
+            
+                
+                       
+
+
+class PipeOld:
     def __init__(self, dir_path:str):
         self.endpoint = Path(dir_path)
 
@@ -35,7 +120,7 @@ class Pipe:
         return f"Pipe('{self.endpoint}')"
 
 
-class InPipe(Pipe):
+class InPipe(PipeOld):
     def take_token(self) -> Token | None:
         try:
             token_path:Path = next(self.endpoint.glob("*.json"))
@@ -56,9 +141,11 @@ class InPipe(Pipe):
         return backup_path
     
 
-class OutPipe(Pipe):
-    def put_token(self, token:Token) -> None:
+class OutPipe(PipeOld):
+    def put_token(self, token:Token, errorflg:bool=False) -> None:
         token_path = self.endpoint / token.name
+        if errorflg:
+            token_path:Path = token_path.with_suffix(".err")
         with open(token_path, 'w') as f:
             json.dump(token.content, f)
 
@@ -72,29 +159,39 @@ class Filter:
     def log_to_token(self, token, level, message):
         token.write_log(message, level, self.stage_name)
 
-    def run_once(self):
-        token = self.input_pipe.take_token()
+    def run_once(self) -> bool:
+        token:Token | None = self.input_pipe.take_token()
 
         if not token:
+            logging.info("No tokens available")
             return False
 
         if self.validate_token(token) is False:
             self.log_to_token(token, "ERROR",
                               "Token did not validate")
+            logging.error("token did not validate")
+            self.output_pipe.put_token(token, errorflg=True)            
+
             return False
 
         try:
             processed:bool = self.process_token(token)
             if processed:
+                logging.info(f"Processed token: {token.name}")
                 self.log_to_token(token, "INFO", "Stage completed successfully")
                 self.output_pipe.put_token(token)
-                logging.info(f"Processed token: {token.name}")
-                return True
             else:
-                self.log_to_token(token, "WARNING", "Stage did not successfully")
+                logging.error(f"Did not proces token: {token.name}")
+                self.log_to_token(token, "WARNING", "Stage did not run successfully")
+                self.output_pipe.put_token(token, errorflg=True)            
+
+            return True
+
+
         except Exception as e:
             self.log_to_token(token, "ERROR", f"in {self.stage_name}: {str(e)}")
             logging.error(f"Error processing {token.name}: {str(e)}")
+            self.output_pipe.put_token(token)
             return False
 
                 
