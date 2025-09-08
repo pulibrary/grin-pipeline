ce454ac91ee3daf80840a703460a59c280bc4756
Cliff Wulfman <cwulfman@princeton.edu>
2025-09-08 17:10:10 -0400

    Implements requester

---
+++ src/pipeline/filters/requester.py
@@ -0,0 +1,49 @@
+import logging
+from pathlib import Path
+from clients import GrinClient
+from pipeline.token_bag import TokenBag
+from pipeline.book_ledger import BookLedger
+from pipeline.plumbing import Pipe, Filter, Token
+
+
+class Requester(Filter):
+    def __init__(self, pipe:Pipe) -> None:
+        super().__init__(pipe)
+        self.grin: GrinClient = GrinClient()
+
+
+    def validate_token(self, token: Token) -> bool:
+        return True
+
+
+    def process_token(self, token: Token) -> bool:
+        successflg = False
+        barcode = token.content["barcode"]
+        response = self.grin.convert_book(barcode)
+        if response is not None:
+            status = response[barcode]
+            self.log_to_token(token, "INFO", status)
+            successflg=True
+        else:
+            logging.error(f"submission of barcode for conversion failed: {barcode}")
+            successflg = False
+
+        return successflg
+
+if __name__ == "__main__":
+    import argparse
+
+    
+    logger: logging.Logger = logging.getLogger(__name__)
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
+
+    pipe: Pipe = Pipe(Path(args.input), Path(args.output))
+
+    requester: Requester = Requester(pipe)
+    logger.info("starting requester")
+    requester.run_forever()
---
+++ tests/test_requester.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import tempfile
+import shutil
+from pipeline.plumbing import Pipe, Token, dump_token
+from pipeline.filters.requester import Requester
+
+
+def test_requester():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        pipe_in = Path(tmpdir) / "in"
+        pipe_out = Path(tmpdir) / "out"
+
+        pipe_in.mkdir()
+        pipe_out.mkdir()
+
+        pipe = Pipe(pipe_in, pipe_out)
+
+        tok = Token({"barcode" : "12345" })
+        dump_token(tok, pipe_in)
+        
+        
+        requester = Requester(pipe)
+
+        requester.run_once()
+        assert len(list(pipe.input.glob("*.*"))) == 0
+        assert len(list(pipe.output.glob("*.*"))) == 1
+
