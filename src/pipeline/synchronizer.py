35203a7163564f547652d11be6d7016206a230cc
Cliff Wulfman <cwulfman@princeton.edu>
2025-09-18 18:58:01 -0400

    overhauls synchronizer

--- src/pipeline/synchronizer.py
+++ src/pipeline/synchronizer.py
@@ -11,14 +11,24 @@ from pipeline.filters.monitors import RequestMonitor
 
 
 class Synchronizer:
+    """A helper object. Sometimes, especially during development, the
+    GRIN converted list gets out of sync with our pipeline.  This
+    object reads that list and chooses those barcodes from the ledger
+    and puts them in the token bag. The stage flag on the synchronize
+    method controls whether the tokens stay in the bag or are staged
+    directly to the converted_bucket.
+    """
+
     def __init__(self, config:dict):
         self.config = config
         self.ledger = BookLedger(config['global']['ledger_file'])
         self.token_bag = TokenBag(config['global']['token_bag'])
         self.secretary = Secretary(self.token_bag, self.ledger)
         processing_bucket = Path(config['global']['processing_bucket'])
-        start_bucket = Path([bucket['path'] for bucket in self.config['buckets'] if bucket['name'] == 'start'][0])        
-        self.stager = Stager(self.secretary, processing_bucket, start_bucket)
+        start_bucket = Path([bucket['path'] for bucket in self.config['buckets'] if bucket['name'] == 'start'][0])
+        converted_bucket = Path([bucket['path'] for bucket in self.config['buckets'] if bucket['name'] == 'converted'][0])        
+        # self.stager = Stager(self.secretary, processing_bucket, start_bucket)
+        self.stager = Stager(self.secretary, processing_bucket, converted_bucket)
         self.pipeline = Pipeline(config)
         self.request_monitor = RequestMonitor(self.pipeline)
         self.client = GrinClient()
@@ -31,11 +41,14 @@ class Synchronizer:
                     if rec['barcode'] not in already_chosen_barcodes]
         return unchosen
         
-    def synchronize(self):
+    def synchronize(self, stage:bool=True):
         chosen = []
-        for barcode in self.out_of_sync_barcodes:
+        # for barcode in self.out_of_sync_barcodes:
+        barcodes = [rec['barcode'] for rec in self.client.converted_books]
+        for barcode in barcodes:
             chosen.append(self.secretary.choose_book(barcode))
-        self.secretary.commit()
-        self.stager.stage()
         
+        self.secretary.commit()
+        if stage is True:
+            self.stager.stage()
         return chosen
