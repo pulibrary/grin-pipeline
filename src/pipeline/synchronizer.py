4cdb5e0d8c9de3acfb020c8c3b5d5ce03b12ca2f
Cliff Wulfman <cwulfman@princeton.edu>
2025-09-19 15:46:56 -0400

    Documenting synchronizer.py

--- src/pipeline/synchronizer.py
+++ src/pipeline/synchronizer.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from clients import GrinClient
 from pipeline.plumbing import Pipeline
 from pipeline.token_bag import TokenBag
-from pipeline.book_ledger import BookLedger
+from pipeline.book_ledger import BookLedger, Book
 from pipeline.stager import Stager
 from pipeline.secretary import Secretary
 from pipeline.filters.monitors import RequestMonitor
@@ -18,34 +18,36 @@ class Synchronizer:
     directly to the converted_bucket.
     """
 
-    def __init__(self, config: dict):
+    def __init__(self, config: dict) -> None:
         self.config = config
         self.ledger = BookLedger(config["global"]["ledger_file"])
         self.token_bag = TokenBag(config["global"]["token_bag"])
         self.secretary = Secretary(self.token_bag, self.ledger)
         processing_bucket = Path(config["global"]["processing_bucket"])
-        start_bucket = Path(
-            [
-                bucket["path"]
-                for bucket in self.config["buckets"]
-                if bucket["name"] == "start"
-            ][0]
-        )
-        converted_bucket = Path(
+
+        # Don't stage to the start bucket as usual; stage to the converted bucket
+        pipeline_bucket = Path(
             [
                 bucket["path"]
                 for bucket in self.config["buckets"]
                 if bucket["name"] == "converted"
             ][0]
         )
-        # self.stager = Stager(self.secretary, processing_bucket, start_bucket)
-        self.stager = Stager(self.secretary, processing_bucket, converted_bucket)
+        self.stager = Stager(self.secretary, processing_bucket, pipeline_bucket)
         self.pipeline = Pipeline(config)
         self.request_monitor = RequestMonitor(self.pipeline)
         self.client = GrinClient()
 
     @property
-    def out_of_sync_barcodes(self):
+    def out_of_sync_barcodes(self) -> list[str] | None:
+        """
+        Retrieve the list of converted books from GRIN.
+        Compare them with those books already marked as chosen;
+        return the list of barcodes that are in GRIN's converted
+        books list but have not been chosen.
+        :return: list of out-of-sync barcodes
+        :rtype: list[str] | None
+        """
         already_chosen_barcodes: list[str] | None = [
             book.barcode for book in self.secretary.chosen_books
         ]
@@ -56,10 +58,23 @@ class Synchronizer:
         ]
         return unchosen
 
-    def synchronize(self, stage: bool = True):
-        chosen = []
-        # for barcode in self.out_of_sync_barcodes:
-        barcodes = [rec["barcode"] for rec in self.client.converted_books]
+    def synchronize(self, out_of_sync_only:bool = False, stage: bool = True) -> list[Book] | None:
+        """
+        Synchronize ledger and bag with the converted books in GRIN.
+
+        :param bool out_of_sync_only: If true, choose only books that haven't already been chosen;
+        if false, choose all books in the converted books list from GRIN.
+        :param bool stage: If true, stage the chosen books to the chosen pipeline bucket.
+        :return: list of chosen books
+        :rtype: list[Book] | None
+        """
+        
+        if out_of_sync_only is True:
+            barcodes = self.out_of_sync_barcodes
+        else:
+            barcodes = [rec["barcode"] for rec in self.client.converted_books]
+
+        chosen:list[Book] | None = []
         for barcode in barcodes:
             chosen.append(self.secretary.choose_book(barcode))
 
